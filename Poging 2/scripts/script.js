823ad22da8d4399e839433b79b0962afcb52e247
RoosJansen <roos.jansen@imagetwo.nl>
2017-10-27 16:42:11 +0200

    wijzigingen

---
+++ Poging 2/scripts/script.js
@@ -0,0 +1,29 @@
+console.log("heeeeee");
+
+/*var legend = document.querySelector('form legend.non');
+console.log(legend);
+
+var button = document.querySelector('fieldset > button:nth-of-type(1)');
+console.log(button);
+
+var open = function() {
+    legend.classList.toggle("on");
+};
+
+ button.addEventListener('click', open);
+
+console.log(open);
+*/
+
+
+// Favorieten 
+
+var favorietButton = document.getElementsByClassName("heart");
+console.log(favorietButton);
+
+favorietButton.addEventListener('click', function(){
+    favorietButton.classList.toggle('faved');
+});
+
+
+
