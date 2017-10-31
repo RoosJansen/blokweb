6fbcc99c5b8c292c6e4bb93ed839a1a34426a7eb
RoosJansen <roos.jansen@imagetwo.nl>
2017-10-31 04:12:12 +0100

    heleboel

--- Poging 2/scripts/script.js
+++ Poging 2/scripts/script.js
@@ -1,4 +1,9 @@
+//Lint error
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
 console.log("heeeeee");
+
+// FILTER POGING 1
 
 /*var legend = document.querySelector('form legend.non');
 console.log(legend);
@@ -13,10 +18,10 @@ var open = function() {
  button.addEventListener('click', open);
 
 console.log(open);
-*/
 
 
-// Favorieten 
+
+// FILTER POGING 2
 
 var filterButton = document.getElementById('checkbox');
 console.log(filterButton);
@@ -28,6 +33,51 @@ var toggleMenu = function () {
 };
 
 filterButton.addEventListener('click', toggleMenu);
+
+*/
+
+//FAVORIET
+/*
+var hartButton = document.getElementById('heart');
+var hart1 = ('../images/microhart1.png');
+var hart2 = ('../images/microhart2.png');
+var hart3 = ('../images/microhart3.png');
+var hart4 = ('../images/microhart4.png');
+
+function vullendHart() {
+  hartButton.innerHTML = hart1;
+  setTimeout(function () {
+    hartButton.innerHTML = hart2;
+  }, 1000);
+  setTimeout(function () {
+    hartButton.innerHTML = hart3;
+  }, 2000);
+  setTimeout(function () {
+    hartButton.innerHTML = hart4;
+  }, 3000);
+}
+
+hartButton.addEventListener('click', vullendHart);
+*/
+
+
+var vulHart = document.getElementsByClassName('hidden');
+var vulHartje = vulHart;
+console.log(vulHart);
+
+/*
+var favoriet = function() {
+    vulHart.classList.remove("hidden");
+    vulHart.classList.add("active");
+};
+*/
+
+function favoriet () {
+    vulHartje.classList.toggle('active');
+    vulHartje.addEventListener('click', favoriet);
+}
+
+console.log(vulHartje);
 
 
 
